eaea12f9e006586e34c958eb73bc506741a78c16
Gleb Bufin <gleb.bufin@paybis.com>
2023-02-26 09:28:04 +0200

    my code is perfect

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,10 @@
+<script setup>
+const user = {};
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
+    <button @click="user.email.send()">Send email</button>
   </div>
 </template>
